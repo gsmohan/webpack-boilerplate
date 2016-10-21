c603a5ef1daf728274b6eac015a2e6a66a860043
gsmohan <mohangs89@gmail.com>
2016-10-21 11:57:01 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,46 @@
 module.exports = {
-  entry: "./app/main.js",
+  entry: './app/main.js',
   output: {
     path: __dirname,
-    filename: "bundle.js"
+    filename: 'bundle.js'
   },
-  module: {
-    loaders:[
-      {test:/\.scss$/, loader:"style!css!sass"},
-      { test: /\.(jpe?g|png|gif|svg)$/i, loader: 'file-loader?limit=10000!image-webpack-loader?progressive=true' }
+  module:{
+    preLoaders: [
+      {
+        test:/\.js$/,
+        exclude: /node_modules/,
+        loader: 'jshint-loader'
+      }
+    ],
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query:{
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'style!css!sass'
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        include: /images/,
+        loader: 'url'
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'url-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}'
+        ]
+      }
     ]
+  },
+  jshint: {
+    esversion: 6
   }
-};
+}
